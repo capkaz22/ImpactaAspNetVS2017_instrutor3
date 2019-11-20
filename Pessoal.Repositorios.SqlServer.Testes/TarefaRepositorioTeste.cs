0328c02622ecb4a69d8b538a197afcef888bbd03
Carlos <capkaz22@gmail.com>
2019-11-19 23:08:13 -0200

    Continuação da aula

--- Pessoal.Repositorios.SqlServer.Testes/TarefaRepositorioTeste.cs
+++ Pessoal.Repositorios.SqlServer.Testes/TarefaRepositorioTeste.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pessoal.Dominio.Entidades;
 
 namespace Pessoal.Repositorios.SqlServer.Testes
 {
@@ -7,19 +8,74 @@ namespace Pessoal.Repositorios.SqlServer.Testes
     public class TarefaRepositorioTeste
     {
         ///preciso fazer a referencia do projeto adicionando os projetos Pessoal.dominio e Pessoal.Repositorio 
-        //fazer o nuget do projeto para instalar o componente microsoft.extensions.configuration
+        //fazer o nuget do projeto para instalar o componente microsoft.extensions.configuration.json
         private TarefaRepositorio repositorio;//field
 
         public TarefaRepositorioTeste()
         {
-            var config = new ConfigurationBuilder()
+            //abrir o nuget instalar o Microsoft.Extensions.Configuration.json
+            //fazer no arquivo appsettings o ajuste dando F4 e alterando para copiar para diretorio -> copiar se for mais novo
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
                 
-            repositorio = new TarefaRepositorio();
+            repositorio = new TarefaRepositorio(config.GetConnectionString("pessoalSqlServer"));
         }
 
         [TestMethod]
         public void InserirTeste()
         {
+            var tarefa = new Tarefa();
+            tarefa.Nome = "Limpar a casa";
+            tarefa.Observacao = "Passar pano na casa toda";
+            tarefa.Prioridade = Prioridade.Alta;
+            tarefa.Concluida = true;
+
+            tarefa.Id = repositorio.Inserir(tarefa);
+
+            Assert.IsTrue(tarefa.Id > 0);
+
+        }
+
+        [TestMethod]
+        public void SelecionarTodosTeste()
+        {
+            Assert.IsTrue(repositorio.Selecionar().Count > 0);
+
+        }
+
+        [TestMethod]
+        public void SelecionarPorIdTeste()
+        {
+            var tarefa = repositorio.Selecionar(1);
+
+            Assert.IsNotNull(repositorio.Selecionar(1));
+            Assert.IsNull(repositorio.Selecionar(0));
+
+
+
+        }
+
+        [TestMethod]
+        public void AtualizarTeste()
+        {
+            var tarefa = new Tarefa();
+            tarefa.Id = 1;
+            tarefa.Nome = "Limpar a caaaaaasa";
+            tarefa.Observacao = "Passar pano na casa todaaaaaaaa";
+            tarefa.Prioridade = Prioridade.Baixa;
+            tarefa.Concluida = true;
+            repositorio.Atualizar(tarefa);
+            tarefa = repositorio.Selecionar(1);
+
+            Assert.IsTrue(tarefa.Id == 1);
+            Assert.AreEqual(tarefa.Prioridade, Prioridade.Baixa);
+
+        }
+
+        [TestMethod]
+        public void ExcluirTeste()
+        {
+            repositorio.Excluir(1);
+            Assert.IsNull(repositorio.Selecionar(1));
 
         }
     }
--- Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
+++ Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
@@ -11,20 +11,48 @@ namespace Pessoal.Repositorios.SqlServer
     {
         private string stringConexao;
 
-        public TarefaRepositorio(string stringConexao)//metodo construtor da classe
-            //obriga passar a string de conexão se não o build quebra
+        public TarefaRepositorio(string stringConexao)
+            
         {
-            this.stringConexao = stringConexao;//usar o this cria o vinculo com a classe em que estou
+            this.stringConexao = stringConexao;
         }
 
         public void Atualizar(Tarefa tarefa)
         {
-            throw new NotImplementedException();
+            using (var conexao = new SqlConnection(stringConexao))                                                                  
+            {
+                conexao.Open();
+                using (var comando = new SqlCommand("TarefaAtualizar", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddRange(Mapear(tarefa).ToArray());
+
+                    
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+
         }
 
         public void Excluir(int id)
         {
-            throw new NotImplementedException();
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = new SqlCommand("TarefaExcluir", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddWithValue("@id", id);
+
+
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+
         }
 
         public int Inserir(Tarefa tarefa)
@@ -39,7 +67,6 @@ namespace Pessoal.Repositorios.SqlServer
                     comando.CommandType = CommandType.StoredProcedure;//CommandType é o enumerador da microsoft
                     comando.Parameters.AddRange(Mapear(tarefa).ToArray());
 
-
                     //comando.ExecuteNonQuery(); não retorna nada
                     return (int)comando.ExecuteScalar();//retorna uma única informação do tipo object extraindo um inteiro
                     
@@ -53,23 +80,83 @@ namespace Pessoal.Repositorios.SqlServer
         {
             var parametros = new List<SqlParameter>();
 
+            if (tarefa.Id != 0)
+            {
+                parametros.Add(new SqlParameter("@Id", tarefa.Id));
+
+            }
+
             parametros.Add(new SqlParameter("@nome",tarefa.Nome));///instancia da classe SqlParameter
             parametros.Add(new SqlParameter("@Prioridade", tarefa.Prioridade));
             parametros.Add(new SqlParameter("@Concluida", tarefa.Concluida));
             parametros.Add(new SqlParameter("@Observacao", tarefa.Observacao));
-
             return parametros;
-
         }
 
         public List<Tarefa> Selecionar()
         {
-            throw new NotImplementedException();
+            var tarefas = new List<Tarefa>();
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = new SqlCommand("TarefaSelecionar", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        while (registro.Read())
+                        {
+                            tarefas.Add(Mapear(registro));
+
+                        }
+                    }
+                }
+
+            }
+
+            return tarefas;
+        }
+
+        private Tarefa Mapear(SqlDataReader registro)
+        {
+            var tarefa = new Tarefa();
+            tarefa.Id = Convert.ToInt32(registro["id"]);
+            tarefa.Concluida = Convert.ToBoolean(registro["Concluida"]);
+            tarefa.Nome = registro["nome"].ToString();
+            tarefa.Observacao = Convert.ToString(registro["observacao"]);
+            tarefa.Prioridade = (Prioridade)Convert.ToInt32(registro["Prioridade"]);//convert do tipo enumerador
+
+            return tarefa;
         }
 
         public Tarefa Selecionar(int id)
         {
-            throw new NotImplementedException();
+            Tarefa tarefa = null;//para garantir caso tenha valor nulo
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = new SqlCommand("TarefaSelecionar", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddWithValue("@Id",id);
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        if (registro.Read())
+                        {
+                            tarefa = Mapear(registro);
+                        }
+                    }
+                }
+
+            }
+
+
+
+            return tarefa;
         }
+
+
     }
 }
