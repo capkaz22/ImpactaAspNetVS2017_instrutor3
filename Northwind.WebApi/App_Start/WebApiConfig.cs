6833a5b43ff1892d888114cb6f9b1ed6cac4ae29
Carlos <capkaz22@gmail.com>
2019-11-26 23:45:27 -0200

    novo projeto api

---
+++ Northwind.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+
+namespace Northwind.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Serviços e configuração da API da Web
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //inserir a primeira letra minuscula
+
+            // Rotas da API da Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
